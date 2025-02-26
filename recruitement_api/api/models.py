d919864540695168d8018f624d99e449593acbba
yazid-911 <m.bouhamidi@ecoles-epsi.net>
2025-02-26 09:44:48 +0100

    feat : creating models

--- recruitement_api/api/models.py
+++ recruitement_api/api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Candidate(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255) 
+    skills = models.TextField(blank=True)
+    def __str__(self):
+        return self.name
+
+class Recruiter(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)  
+    company_name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
