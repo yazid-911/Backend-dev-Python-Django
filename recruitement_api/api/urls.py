acf1b8550bfe19009cbf7dadcdc58836bebc05c3
yazid-911 <m.bouhamidi@ecoles-epsi.net>
2025-02-28 14:03:35 +0100

    feat : recruiters ability to get candidates credentials, and swagger open API documentation

--- recruitement_api/api/urls.py
+++ recruitement_api/api/urls.py
@@ -5,12 +5,35 @@ from .views import (
     RecruiterListCreate,
     RecruiterRetrieveUpdateDestroy
 )
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from .views import CandidateListForRecruiters
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+
 
 urlpatterns = [
     path('candidates/', CandidateListCreate.as_view(), name='candidate-list-create'),
     path('candidates/<int:pk>/', CandidateRetrieveUpdateDestroy.as_view(), name='candidate-detail'),
 
     path('recruiters/', RecruiterListCreate.as_view(), name='recruiter-list-create'),
-    path('recruiters/', CandidateListCreate.as_view(), name='recruiter-list-create'),
     path('recruiters/<int:pk>/', RecruiterRetrieveUpdateDestroy.as_view(), name='recruiter-detail'),
+    path('swagger/schema/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('swagger/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-ui'),
+     path('recruiters/candidates/', CandidateListForRecruiters.as_view(), name='recruiters-candidates-list'),
+
 ]
--- recruitement_api/api/views.py
+++ recruitement_api/api/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Candidate, Recruiter
 from .serializers import CandidateSerializer, RecruiterSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 
 class CandidateListCreate(generics.ListCreateAPIView):
     queryset = Candidate.objects.all()
@@ -16,10 +19,18 @@ class CandidateRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
 class RecruiterListCreate(generics.ListCreateAPIView):
     queryset = Recruiter.objects.all()
     serializer_class = RecruiterSerializer
-    queryset = Candidate.objects.all()
-    serializer_class = CandidateSerializer
     
 class RecruiterRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Recruiter.objects.all()
     serializer_class = RecruiterSerializer
     lookup_field = "pk"
+
+class CandidateListForRecruiters(APIView):
+
+    def get(self, request):
+        candidates = Candidate.objects.all()  # Récupère tous les candidats
+        serializer = CandidateSerializer(candidates, many=True)  # Sérialisation
+        return Response(serializer.data)
+
+
+    
